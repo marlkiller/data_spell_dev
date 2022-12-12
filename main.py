ca52f505d7eead23c5a0e5f1c6cc10ef4ae4045c
root <root@ip-172-26-9-224.ap-southeast-1.compute.internal>
2022-12-12 11:21:50 +0800

    dev

--- main.py
+++ main.py
@@ -2,6 +2,26 @@
 pip3 install jupyter
 pip3 install pandas
 pip3 install matplotlib
+
+[root@ip-172-26-9-224 data_spell_dev]# find  / -name jupyter
+/opt/rh/rh-python38/root/usr/local/bin/jupyter
+/opt/rh/rh-python38/root/usr/local/etc/jupyter
+/opt/rh/rh-python38/root/usr/local/share/jupyter
+
+export PATH=$PATH:/opt/rh/rh-python38/root/usr/local/bin
+
+
+
+jupyter notebook --generate-config
+Writing default config to: /root/.jupyter/jupyter_notebook_config.py
+
+
+    修改端口 c.NotebookApp.port = 8888
+    修改可连接的服务器 c.NotebookApp.ip='*'
+
+jupyter notebook --allow-root
+jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root
+
 """
 if __name__ == '__main__':
     import pandas as pd
